cb0a43a4e3eee7908000bff9cb15cc69d8188647
Abdul Rahman <abdulnoorisys2@gmail.com>
2023-04-27 20:46:33 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { SortPipe } from './shared/sort.pipe';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from './shared/search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    SortPipe,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
