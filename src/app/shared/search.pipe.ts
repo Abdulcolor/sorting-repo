7423ee69af867812883994eb56834cf20920b92c
Abdul Rahman <abdulnoorisys2@gmail.com>
2023-04-28 12:07:43 +0530

    changes in pipe

--- src/app/shared/search.pipe.ts
+++ src/app/shared/search.pipe.ts
@@ -6,12 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(value: any, args: any): any {
-    console.log('value', value);
-    console.log('args', args);
-    
-    // return value.filter((employee)=>{
-    //   return employee.first==args
-    // });
     if(!value)return null;
         if(!args)return value;
 
